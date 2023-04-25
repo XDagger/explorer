494fa4a20aff2b390ab862d8ce230ad5e0505d42
Viliam Kubis <viliam.kubis@gmail.com>
2023-04-25 20:35:08 +0200

    update main block colors key dialog

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -174,7 +174,11 @@
 											@foreach ($mainBlocks->pluck('remark')->unique()->sort() as $mainBlockRemark)
 												<div class="mt-4 px-2 flex items-center w-full sm:w-1/2">
 													<div class="bg-grey-lighter w-3 h-3 shadow rounded-full mr-2" style="background-color: {{ $mainBlockRemark !== null ? color($mainBlockRemark) : 'auto' }}"></div>
-													<span>{{ $mainBlockRemark ?? 'No pool tag' }}</span>
+													@if (($remarkLink = firstLink($mainBlockRemark)) !== null)
+														<a href="{{ $remarkLink }}" target="_blank" style="color: inherit">{{ linksToDomains($mainBlockRemark) }}</a>
+													@else
+														{{ $mainBlockRemark ?? 'No pool tag' }}
+													@endif
 												</div>
 											@endforeach
 										</div>
